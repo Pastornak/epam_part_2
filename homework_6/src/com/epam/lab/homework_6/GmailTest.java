aaed61eba65d728d14a836c6a59836e6efdcd0c6
Yurii Pasternak <yurapaster@gmail.com>
2018-09-28 20:56:28 +0300

    Finished the project, test runs 5 times with 3 threads, also one driver instance for each thread

--- homework_6/src/com/epam/lab/homework_6/GmailTest.java
+++ homework_6/src/com/epam/lab/homework_6/GmailTest.java
@@ -1,18 +1,14 @@
 package com.epam.lab.homework_6;
 
-import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.epam.lab.homework_6.business_objects.GmailBO;
+import com.epam.lab.homework_6.drivers.ChromeDriverPool;
 import com.epam.lab.homework_6.readers.*;
 import com.epam.lab.homework_6.users_emails.*;
 import com.epam.lab.homework_6.utils.Pair;
@@ -21,32 +17,17 @@ public class GmailTest {
 	
 	private static final Logger LOG = Logger.getLogger(GmailTest.class);
 	
-	private int implicitTimeWait;
 	private String pathToChromeDriver;
-	
 	private UserEmailPairs userEmailPairs;
-	
-	//private WebDriver driver;
 	
 	@BeforeClass
 	public void setUp(){
+		LOG.debug("BeforeClass");
 		PropertiesParser pp = new PropertiesParser("resources/driver_config.properties");
-		implicitTimeWait = pp.getImplicitWaitTimeProperty();
 		pathToChromeDriver = pp.getChromeDriverPath();
 		System.setProperty("webdriver.chrome.driver", pathToChromeDriver);
 	
 		userEmailPairs = new UserEmailPairs();
-	}
-
-	@BeforeMethod
-	public void setUpDriver() {
-//		driver = new ChromeDriver();
-//		driver.manage().timeouts().implicitlyWait(implicitTimeWait, TimeUnit.SECONDS);
-	}
-	
-	@AfterMethod
-	public void tearDownDriver(){
-		//driver.quit();
 	}
 	
 	@DataProvider(name = "user-email", parallel=true)
@@ -57,21 +38,17 @@ public class GmailTest {
 		};
 	}
 	
-	@Test(dataProvider = "user-email", threadPoolSize = 3, invocationCount = 3)
+	@Test(dataProvider = "user-email", threadPoolSize = 3, invocationCount = 5)
 	public void testGmailSendEmail(User user, Email email){
+		LOG.debug("Testing: testGmailSendEmail, input: [" + user.toString() + "; " + email.toString() +"]");
 		String login = user.getLogin();
 		String password = user.getPassword();
-		
 		String emailTo = email.getTo();
 		String emailSubject = email.getSubject();
 		String emailText = email.getText();
 		
-		LOG.debug("Creating driver");
-		WebDriver driver = new ChromeDriver();
-		LOG.debug("Setting implicit wait time");
-		driver.manage().timeouts().implicitlyWait(implicitTimeWait, TimeUnit.SECONDS);
 		LOG.debug("Creating gmail Business Object");
-		GmailBO gmailBO = new GmailBO(driver);
+		GmailBO gmailBO = new GmailBO(ChromeDriverPool.getInstance());
 		LOG.debug("Login into gmail");
 		gmailBO.login(login, password);
 		LOG.debug("Writing email");
@@ -83,6 +60,11 @@ public class GmailTest {
 		LOG.debug("Deleting sent email");
 		Assert.assertTrue(gmailBO.deleteSentEmail(1));
 		LOG.debug("Quitting driver");
-		driver.quit();
+	}
+	
+	@AfterMethod
+	public void cleanUp(){
+		LOG.debug("AfterMethod, Closing thread's driver");
+		ChromeDriverPool.getInstance().quit();
 	}
 }
---
+++ homework_6/src/com/epam/lab/homework_6/drivers/ChromeDriverPool.java
@@ -0,0 +1,27 @@
+package com.epam.lab.homework_6.drivers;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.epam.lab.homework_6.readers.PropertiesParser;
+
+public class ChromeDriverPool {
+
+	private ChromeDriverPool(){}
+	
+	private static final ThreadLocal<WebDriver> localValue = new ThreadLocal<WebDriver>(){
+		protected WebDriver initialValue(){
+			WebDriver driver = new ChromeDriver();
+			int implicitTimeWait = (new PropertiesParser("resources/driver_config.properties"))
+					.getImplicitWaitTimeProperty();
+			driver.manage().timeouts().implicitlyWait(implicitTimeWait, TimeUnit.SECONDS);
+			return driver;
+		}
+	};
+	
+	public static WebDriver getInstance(){
+		return localValue.get();
+	}
+}
